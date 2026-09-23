0e1d65fcd87314ff7e53499fa98f21c425de6e46
kirakein-gif <kirakein@gmail.com>
2026-09-23 10:24:34 +0900

    feat: add access gate configuration

--- app/config.py
+++ app/config.py
@@ -3,3 +3,9 @@ KAKAO_REST_API_KEY = os.getenv("KAKAO_REST_API_KEY", "")
 OPINET_API_KEY = os.getenv("OPINET_API_KEY", "")
 GCP_PROJECT_ID = os.getenv("GCP_PROJECT_ID", "")
 EVIDENCE_BUCKET = os.getenv("EVIDENCE_BUCKET", "")
+
+ACCESS_CONTROL_ENABLED = os.getenv("ACCESS_CONTROL_ENABLED", "false").strip().lower() in {
+    "1", "true", "yes", "on"
+}
+OWNER_ACCESS_KEY = os.getenv("OWNER_ACCESS_KEY", "")
+ACCESS_COOKIE_NAME = os.getenv("ACCESS_COOKIE_NAME", "ddalkkak_access")
